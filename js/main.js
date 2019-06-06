49f39eefbace24b7edbdfb4111fca8cc79ff40e4
k-Knight <28106261+k-Knight@users.noreply.github.com>
2019-06-06 21:48:09 +0200

    Trying to add push notifications

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var api_key = "b67824da4cc7b16eb40d000f7c26a2c7";
 var base_url = "https://ws.audioscrobbler.com/2.0/";
+var pushEnabled = false
 
 function displayLoading(msg, container) {
     $(container).empty();
@@ -18,4 +19,39 @@ function displayFaded(msg, container) {
 
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register ('service-worker.js');
+
+
+
+}
+
+if ('showNotification' in ServiceWorkerRegistration.prototype) {
+    if (Notification.permission === 'denied') {
+        if ('PushManager' in window) {
+            navigator.serviceWorker.ready.then((serviceWorkerRegistration) => {
+                serviceWorkerRegistration.pushManager.getSubscription().then((subscription) => {
+                    subscribeToPush();
+
+                    //pushEnabled = true;
+                }).catch(function(err) {
+                    console.warn('Error during getSubscription()', err);
+                });
+            });
+            console.log("Push notifications are available");
+        }
+    }
+}
+
+function subscribeToPush() {
+  navigator.serviceWorker.ready.then((serviceWorkerRegistration) => {
+    serviceWorkerRegistration.pushManager.subscribe().then((subscription) => {
+        // The subscription was successful
+        pushEnabled = true;
+    }).catch((e) => {
+        if (Notification.permission === 'denied') {
+          console.warn('Permission for Notifications was denied');
+        } else {
+          console.error('Unable to subscribe to push.', e);
+        }
+    });
+  });
 }
