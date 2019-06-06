807441c44b2350249b2f34f1e23200fe983d5573
k-Knight <28106261+k-Knight@users.noreply.github.com>
2019-06-06 17:32:06 +0200

    Fixed issues with mixed content and API inconsistency

--- js/albums.js
+++ js/albums.js
@@ -2,45 +2,62 @@ window.onload = function () {
     savedData = window.localStorage.getItem("top-albums");
     if (savedData == null || savedData == "undefined") {
         displayLoading("Please wait. Loading...", "#loading-container");
-        $.getJSON(base_url + "?method=tag.gettopalbums&tag=grindcore&api_key=" + api_key + "&limit=21&format=json", ( data ) => {
-            try {
-                $.each( data.albums.album, ( key, val ) => {
-                    var element = $("<section>").addClass("album-element").append([
-                        $("<h3>")
-                            .addClass("info-element-head")
-                            .text(val.name),
-                        $("<div>")
-                            .addClass("album-info")
-                            .append([
-                                $("<img>")
-                                    .attr({
-                                        alt: "cover art",
-                                        src: val.image[3]["#text"]
-                                    })
-                                    .addClass("info-element-img"),
-                                $("<table>").append([
-                                    $("<tr>").append([
-                                            $("<td>").addClass("info-element-faded").text("Name"),
-                                            $("<td>").append($("<div>").text(val.name))
-                                        ]),
-                                    $("<tr>").append([
-                                            $("<td>").addClass("info-element-faded").text("By"),
-                                            $("<td>").append($("<div>").text(val.artist.name))
-                                        ])
-                                ])
-                            ])
-                    ]);
-
-                    $("#loading-container").empty();
-                    $("#top-albums-container").append(element);
-                    window.localStorage.setItem("top-albums", $("#top-albums-container").html());
-                });
-            } catch (err) {
-                console.log(err);
-            }
-        });
+        getAlbumsInfo();
     }
     else {
         $("#top-albums-container").append(savedData);
+    }
+}
+
+function getAlbumsInfo() {
+    $.getJSON({
+        url: base_url + "?method=tag.gettopalbums&tag=grindcore&api_key=" + api_key + "&limit=21&format=json",
+        success: (data) => { displayAlbums(data, () => { getAlbumsInfo(); }); },
+        complete: (xhr) => {
+            if (xhr.status != 200)
+                getAlbumsInfo();
+        }
+    });
+}
+
+function displayAlbums(data, callback) {
+    if (typeof data != "object" && typeof callback == "function") {
+        callback();
+        return;
+    }
+    try {
+        $.each( data.albums.album, ( key, val ) => {
+            var element = $("<section>").addClass("album-element").append([
+                $("<h3>")
+                    .addClass("info-element-head")
+                    .text(val.name),
+                $("<div>")
+                    .addClass("album-info")
+                    .append([
+                        $("<img>")
+                            .attr({
+                                alt: "cover art",
+                                src: val.image[3]["#text"]
+                            })
+                            .addClass("info-element-img"),
+                        $("<table>").append([
+                            $("<tr>").append([
+                                    $("<td>").addClass("info-element-faded").text("Name"),
+                                    $("<td>").append($("<div>").text(val.name))
+                                ]),
+                            $("<tr>").append([
+                                    $("<td>").addClass("info-element-faded").text("By"),
+                                    $("<td>").append($("<div>").text(val.artist.name))
+                                ])
+                        ])
+                    ])
+            ]);
+
+            $("#loading-container").empty();
+            $("#top-albums-container").append(element);
+            window.localStorage.setItem("top-albums", $("#top-albums-container").html());
+        });
+    } catch (err) {
+        console.log(err);
     }
 }
--- js/search_artists.js
+++ js/search_artists.js
@@ -1,3 +1,8 @@
+var searching = false;
+var finished = 0;
+var found = 0;
+var potential = 0;
+
 function getURLParameter(sParam) {
     var sPageURL = window.location.search.substring(1);
     var sURLVariables = sPageURL.split('&');
@@ -13,66 +18,128 @@ function getURLParameter(sParam) {
 window.onload = function() {
     var searchItem = getURLParameter("artistName");
     if (searchItem != null && searchItem != "") {
-        var searching = true;
+        searching = true;
         displayLoading("Please wait. Searching...", "#artists-container");
-
-        try {
-            $.getJSON(base_url + "?method=artist.search&api_key=" + api_key + "&limit=250&format=json&artist=" + searchItem, ( data ) => {
-                var validArtists = data.results.artistmatches.artist.filter((artist) => { return artist.mbid != null && artist.mbid != ""; });
-                for (var i = 0; i < validArtists.length; i++) {
-                    for (var j = i + 1; j < validArtists.length; j++) {
-                        if (validArtists[i].mbid == validArtists[j].mbid) {
-                            validArtists.splice(j, 1);
-                            j--;
-                        }
-                    }
-                }
-                if (validArtists.length == 0) {
-                    displayFaded("Sorry. No artists were found.", "#artists-container");
-                }
-                var finished = 0;
-                var found = 0;
-                $.each( validArtists, function( key, val ) {
-                    $.getJSON(base_url + "?method=artist.getTopTags&api_key=" + api_key + "&format=json&mbid=" + val.mbid, ( tagData ) => {
-                        grindcoreTags = tagData.toptags.tag.filter((tag) => {
-                            return tag.name.toUpperCase() == "grindcore".toUpperCase() ||
-                                tag.name.toUpperCase() == "deathgrind".toUpperCase() ||
-                                tag.name.toUpperCase() == "goregrind".toUpperCase() ||
-                                tag.name.toUpperCase() == "noisegrind".toUpperCase();
-                        });
-                        if (grindcoreTags.length > 0) {
-                            $.getJSON(base_url + "?method=artist.getinfo&api_key=" + api_key + "&format=json&mbid=" + val.mbid, ( artistData ) => {
-                                var element = $("<div>").addClass("artist-element").append([
-                                    $("<section>").append(
-                                        $("<h3>").text(artistData.artist.name),
-                                        $("<div>").addClass("artit-info").append([
-                                                $("<div>").addClass("info-element-faded").text("Last.fm listeners: "),
-                                                $("<div>").text(artistData.artist.stats.listeners)
-                                            ]),
-                                        $("<div>").append($("<p>").append(artistData.artist.bio.summary))
-                                    )
-                                ]);
-
-                                if (searching == true) {
-                                    searching = false;
-                                    $("#artists-container").empty();
-                                }
-                                $("#artists-container").append(element);
-                            });
-                            found++;
-                        }
-                        finished++;
-                        if (found == 0 && finished == validArtists.length) {
-                            displayNothingFound();
-                        }
-                    });
-                });
-            });
-        } catch (err) {
-            console.log(err);
-        }
+        getArtistsByName(searchItem);
     }
     else {
         displayFaded("Search grindcore artists by name.", "#artists-container");
+    }
+}
+
+function getArtistsByName(name) {
+    $.getJSON({
+        url: base_url + "?method=artist.search&api_key=" + api_key + "&limit=250&format=json&artist=" + name,
+        success: (data) => searchValidArtists(data, () => { getArtistsByName(name); } ),
+        complete: (xhr) => {
+            if (xhr.status != 200)
+                getArtistsByName(name);
+        }
+    });
+}
+
+function searchValidArtists(data, callback) {
+    if (typeof data != "object" && typeof callback == "function") {
+        callback();
+        return;
+    }
+    try {
+        var validArtists = data.results.artistmatches.artist.filter((artist) => { return artist.mbid != null && artist.mbid != ""; });
+        for (var i = 0; i < validArtists.length; i++) {
+            for (var j = i + 1; j < validArtists.length; j++) {
+                if (validArtists[i].mbid == validArtists[j].mbid) {
+                    validArtists.splice(j, 1);
+                    j--;
+                }
+            }
+        }
+        if (validArtists.length == 0) {
+            displayFaded("Sorry. No artists were found.", "#artists-container");
+        }
+        $.each( validArtists, function( key, val ) {
+            potential = validArtists.length;
+            getArtistTags(val.mbid);
+        });
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+function getArtistTags(mbid) {
+    $.getJSON({
+        url: base_url + "?method=artist.getTopTags&api_key=" + api_key + "&format=json&mbid=" + mbid,
+        success: (data) => { confirmGrindcoreArtist(data, mbid, () => { getArtistTags(mbid); }); },
+        complete: (xhr) => {
+            if (xhr.status != 200)
+            getArtistTags(mbid);
+        }
+    });
+}
+
+function confirmGrindcoreArtist(data, mbid, callback) {
+    if (typeof data != "object" && typeof callback == "function") {
+        callback();
+        return;
+    }
+    try {
+        grindcoreTags = data.toptags.tag.filter((tag) => {
+            return tag.name.toUpperCase() == "grindcore".toUpperCase() ||
+                tag.name.toUpperCase() == "deathgrind".toUpperCase() ||
+                tag.name.toUpperCase() == "goregrind".toUpperCase() ||
+                tag.name.toUpperCase() == "noisegrind".toUpperCase();
+        });
+        if (grindcoreTags.length > 0) {
+            getArtistInfo(mbid);
+        }
+        else {
+            console.log({finished: finished, potential: potential});
+            finished++;
+            if (found == 0 && finished >= potential) {
+                displayFaded("Sorry. No artists were found.", "#artists-container");
+            }
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+function getArtistInfo(mbid) {
+    $.getJSON({
+        url: base_url + "?method=artist.getinfo&api_key=" + api_key + "&format=json&mbid=" + mbid,
+        success: displayArtistInfo,
+        complete: () => {
+            finished++;
+            if (found == 0 && finished >= potential) {
+                displayFaded("Sorry. No artists were found.", "#artists-container");
+            }
+        }
+    });
+}
+
+function displayArtistInfo(data, callback) {
+    if (typeof data != "object" && typeof callback == "function") {
+        callback();
+        return;
+    }
+    try {
+        var element = $("<div>").addClass("artist-element").append([
+            $("<section>").append(
+                $("<h3>").text(data.artist.name),
+                $("<div>").addClass("artit-info").append([
+                        $("<div>").addClass("info-element-faded").text("Last.fm listeners: "),
+                        $("<div>").text(data.artist.stats.listeners)
+                    ]),
+                $("<div>").append($("<p>").append(data.artist.bio.summary))
+            )
+        ]);
+
+        if (searching == true) {
+            searching = false;
+            $("#artists-container").empty();
+        }
+        $("#artists-container").append(element);
+        found++;
+    } catch (err) {
+        console.log(err);
     }
 }
