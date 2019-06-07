1d8bc146971777958c73596f0fb24fe909c4343b
k-Knight <28106261+k-Knight@users.noreply.github.com>
2019-06-07 11:32:40 +0200

    fixing worker and caching

--- service-worker.js
+++ service-worker.js
@@ -1,45 +1,45 @@
 const cacheName = "grindcore_cache";
 
 const filesToCache = [
-    "css/kap_style.css",
-    "css/normalize.css",
-    "js/vendor/jquery-3.3.1.min.js",
-    "js/vendor/modernizr-3.7.1.min.js",
-    "js/main.js",
-    "js/plugins.js",
-    "js/albums.js",
-    "js/artists.js",
-    "js/search_artists.js",
-    "img/grind_logo.png",
-    "img/grind_mini.png",
-    "img/loading.gif",
-    "img/np_scum.png",
-    "img/offline.png",
-    "img/search_complete.png",
-    "img/nothing_found.png",
-    "img/icons/icon_16.png",
-    "img/icons/icon_24.png",
-    "img/icons/icon_32.png",
-    "img/icons/icon_48.png",
-    "img/icons/icon_64.png",
-    "img/icons/icon_96.png",
-    "img/icons/icon_128.png",
-    "img/icons/icon_192.png",
-    "img/icons/icon_256.png",
+    "/grindcore.io/css/kap_style.css",
+    "/grindcore.io/css/normalize.css",
+    "/grindcore.io/js/vendor/jquery-3.3.1.min.js",
+    "/grindcore.io/js/vendor/modernizr-3.7.1.min.js",
+    "/grindcore.io/js/main.js",
+    "/grindcore.io/js/plugins.js",
+    "/grindcore.io/js/albums.js",
+    "/grindcore.io/js/artists.js",
+    "/grindcore.io/js/search_artists.js",
+    "/grindcore.io/img/grind_logo.png",
+    "/grindcore.io/img/grind_mini.png",
+    "/grindcore.io/img/loading.gif",
+    "/grindcore.io/img/np_scum.png",
+    "/grindcore.io/img/offline.png",
+    "/grindcore.io/img/search_complete.png",
+    "/grindcore.io/img/nothing_found.png",
+    "/grindcore.io/img/icons/icon_16.png",
+    "/grindcore.io/img/icons/icon_24.png",
+    "/grindcore.io/img/icons/icon_32.png",
+    "/grindcore.io/img/icons/icon_48.png",
+    "/grindcore.io/img/icons/icon_64.png",
+    "/grindcore.io/img/icons/icon_96.png",
+    "/grindcore.io/img/icons/icon_128.png",
+    "/grindcore.io/img/icons/icon_192.png",
+    "/grindcore.io/img/icons/icon_256.png",
     "/grindcore.io/manifest.json",
     "/grindcore.io/service-worker.js"
 ];
 
 const pagesToCache = [
-    "index.html",
-    "top_artists.html",
-    "top_albums.html",
-    "artist_search.html"
+    "/grindcore.io/index.html",
+    "/grindcore.io/top_artists.html",
+    "/grindcore.io/top_albums.html",
+    "/grindcore.io/artist_search.html"
 ];
 
 self.addEventListener("install", (event) => {
     event.waitUntil(caches.open(cacheName).then((cache) => {
-        fetch("manifest.json").then((response) => {
+        fetch("/grindcore.io/manifest.json").then((response) => {
             response.json()
         })
         .then((assets) => {
